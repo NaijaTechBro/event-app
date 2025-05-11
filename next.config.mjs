f0fea7116c0b9813e66a1c01e956c38e0c081ed9
NaijaTechBro <bakisodiq@gmail.com>
2025-05-11 18:05:06 +0100

    update: modified next.config to work with date-fns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+   transpilePackages: ['date-fns'],
+
+  
+};
 
 export default nextConfig;
